341b35d89b1ad515f3077b81ad1a73c71309da10
3wot <j1j2w3@163.com>
2019-01-01 18:14:51 +0800

    Signed-off-by: 3wot <j1j2w3@163.com>

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -8,24 +8,69 @@
 			</el-header> -->
 			<Header title="首页"></Header>
 		  	<el-main class="c-main">
-				<div class="op-item" @click="gotoValuation">
-					<span class="pull-left op-item-icon">
-						<i class="el-icon-menu"></i>
-					</span>
-					<span>估值</span>
-					<span class="pull-right op-item-icon">
-						<i class="el-icon-arrow-right"></i>
-					</span>
-				</div>
-				<div class="op-item" @click="gotoFirstCheck">
-					<span class="pull-left op-item-icon">
-						<i class="el-icon-menu"></i>
-					</span>
-					<span>初审</span>
-					<span class="pull-right op-item-icon">
-						<i class="el-icon-arrow-right"></i>
-					</span>
-				</div>
+		  		<div class="main-in">
+		  			<el-button-group>
+					  	<el-button type="primary" :plain="type != 1" class="btn-100" @click="clickType(1)">今天</el-button>
+					  	<el-button type="primary" :plain="type != 2" class="btn-100" @click="clickType(2)">昨天</el-button>
+					  	<el-button type="primary" :plain="type != 3" class="btn-100" @click="clickType(3)">本月</el-button>
+					</el-button-group>
+					<el-row>
+						<el-col :span="24" class="index-item">
+							<el-date-picker
+								class="index-time"
+						      	v-model="value1"
+						      	type="datetime"
+						      	placeholder="开始时间"
+						      	@change="changeTime"
+						    >
+						    </el-date-picker>
+						</el-col>
+						<el-col :span="24" class="index-item">
+							<el-date-picker
+								class="index-time"
+						      	v-model="value2"
+						      	type="datetime"
+						      	placeholder="结束时间"
+						      	@change="changeTime"
+						    >
+						    </el-date-picker>
+						</el-col>
+						<el-col :span="24" class="index-item">
+							<el-button type="primary" class="btn-300" @click.native="init">查询</el-button>
+						</el-col>
+						<el-col :span="24" class="index-item">
+							<p>
+								<span class="index-num">{{VisitUserNumber}}</span>	
+							</p>
+							<span>
+								访问用户数
+							</span>
+						</el-col>
+
+						<el-col :span="24" class="index-item">
+							<p>
+								<span class="index-num">{{RegUserNumber}}</span>	
+							</p>
+							<span>
+								注册用户数
+							</span>
+						</el-col>
+
+						<el-col :span="24" class="index-item">
+							<p>
+								<span class="index-num">{{ApprovalUserNumber}}</span>	
+							</p>
+							<span>
+								成交用户数
+							</span>
+						</el-col>
+						  	
+						
+					</el-row>	
+
+		  		</div>
+				
+
 		  	</el-main>
 			
 		</el-container>
@@ -46,21 +91,92 @@ export default {
 	name: 'Index',
 	data () {
 		return {
-			
+			type: 3,
+			value1: '',
+			value2: '',
+			VisitUserNumber: 1000,
+			RegUserNumber: 0,
+			ApprovalUserNumber: 0,
 		}
 	},
 	mounted () {
-		
+		this.clickType(3)
 	},
 	methods:{
-		
-		// 估值
-		gotoValuation() {
-			this.$router.push({ name : 'valuation' })
+
+		changeTime() {
+			console.log(this.value1)
 		},
-		// 初审
-		gotoFirstCheck() {
-			this.$router.push({ name : 'firstCheck' })
+
+		clickType(type) {
+			this.type = type
+			const time = new Date()
+			const year = time.getFullYear()
+			const month = time.getMonth()
+			const day = time.getDate()
+			const hour = time.getHours()
+			const min = time.getMinutes()
+			const sec = time.getSeconds()
+			let start
+			let end
+			if (type == 1) {
+				start = new Date(year, month, day, 0, 0, 0)
+				end = time
+			} else if (type == 2) {
+				end = new Date(year, month, day, 0, 0, 0)
+				start = new Date(end.getTime() - 24*3600*1000)
+			} else if (type == 3) {
+				start = new Date(year, month, 1, 0, 0, 0)
+				end = time
+			}
+			console.log(start)
+			console.log(end)
+			this.value1 = start
+			this.value2 = end
+			this.init()
+		},
+
+		format(time) {
+   			if (time) {
+				const year = time.getFullYear()
+				const month = parseInt(time.getMonth()+1)>=10?parseInt(time.getMonth()+1):'0'+parseInt(time.getMonth()+1)
+				const day = parseInt(time.getDate())>=10 ? parseInt(time.getDate()) : '0'+ parseInt(time.getDate())
+				const hour = parseInt(time.getHours())>=10? parseInt(time.getHours()): '0'+parseInt(time.getHours())
+				const min = parseInt(time.getMinutes())>=10? parseInt(time.getMinutes()): '0'+parseInt(time.getMinutes())
+				const sec = parseInt(time.getSeconds())>=10? parseInt(time.getSeconds()): '0'+parseInt(time.getSeconds())
+				return year+'-'+month+'-'+day+' '+hour+':'+min+':'+sec
+   			}
+		},
+
+		init() {
+			const {
+				value1,
+				value2,
+			} = this
+			if (!value1 || !value2) {
+				this.warn("请选择时间！")
+				return
+			}
+			const StartDateTime = this.format(value1)
+			const EndDateTime = this.format(value2)
+			const param = {
+				StartDateTime,
+				EndDateTime,
+			}
+			this.pp('GetStatistics', param, res => {
+				if (res.ret) {
+					const {
+						VisitUserNumber,
+						RegUserNumber,
+						ApprovalUserNumber,
+					} = res.data
+					this.VisitUserNumber = VisitUserNumber
+					this.RegUserNumber = RegUserNumber
+					this.ApprovalUserNumber = ApprovalUserNumber
+				} else {
+					this.warn(res.msg)
+				}
+			})
 		},
 
 	},
@@ -116,7 +232,34 @@ export default {
 .op-item-icon {
 	font-size: 18px;
 }
-
+.c-main {
+	text-align: center;
+}
+.main-in {
+	width: 300px;
+	text-align: center;
+	margin: 0 auto;
+}
+.index-item {
+	margin-top: 20px;
+}
+.index-time {
+	width: 100%;
+}
+.btn-300 {
+	width: 300px;
+}
+.index-num {
+	display: inline-block;
+	width: 50px;
+	height: 50px;
+	line-height: 50px;
+	border-radius: 25px;
+	background-color: #409eff;
+	color: #ffffff;
+	font-size: 20px;
+	font-weight: bold;
+}
 
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Index from '@/components/Index'
-import Valuation from '@/components/Valuation'
-import FirstCheck from '@/components/FirstCheck'
-import FirstDetail from '@/components/FirstDetail'
 
 Vue.use(Router)
 
@@ -22,25 +19,7 @@ export default new Router({
 			path: '/index',
 			name: 'index',
 			component: Index
-		},
-		{
-			// 估值
-			path: '/valuation',
-			name: 'valuation',
-			component: Valuation
-		},
-		{
-			// 初审
-			path: '/firstCheck',
-			name: 'firstCheck',
-			component: FirstCheck
-		},
-		{
-			// 详情
-			path: '/firstDetail/:name/:id?',
-			name: 'firstDetail',
-			component: FirstDetail
-		},
+		}
 		
 		
 	]
